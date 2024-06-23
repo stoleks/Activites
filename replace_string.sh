e4b4def25d9d253e37270e4f4a3d04bb62a56059
stoleks <stoleks@hotmail.fr>
2024-06-23 23:50:57 +0200

    Modification du nom de la boite encart -> importants.
    Modification des fichiers utilisant la commande.

--- replace_string.sh
+++ replace_string.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# replace string in chapter dir
 for entry in $1/C*/*.tex ; do
-  # echo $entry
+  if grep -q -Fwi $2 $entry ; then
+    echo "replace $2 in $entry"
+    sed -i "s@$2@$3@g" $entry
+  fi
+done
+
+# replace string in "root" dir
+for entry in $1/*.tex ; do
   if grep -q -Fwi $2 $entry ; then
     echo "replace $2 in $entry"
     sed -i "s@$2@$3@g" $entry
